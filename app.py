f75df2cd7c542f22f0ab1498b0f5db1fc6d7cceb
Prajeeth <prajeeth347@gmail.com>
2023-06-01 22:39:35 +0530

    fixing

--- app.py
+++ app.py
@@ -1,43 +1,36 @@
-import json
-from flask import Flask, request, jsonify
-import base64
-import whisper
+from flask import Flask,request,jsonify
 from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate
+import whisper
+import json
+import base64
+from googletrans import Translator
+
 model = whisper.load_model("small")
+translator = Translator()
 
 def transcribe(audio):
-    
-    # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio(audio)
     audio = whisper.pad_or_trim(audio)
-
-    # make log-Mel spectrogram and move to the same device as the model
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    # detect the spoken language
-    # _, probs = model.detect_language(mel)
-
-    # decode the audio
     options = whisper.DecodingOptions(fp16 = False,language="hi")
     result = whisper.decode(model, mel, options)
     return result.text
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route('/hello/', methods=['POST'])
+@app.route('/',methods = ["POST"])
 def welcome():
-    record = json.loads(request.data)
-    record = record['character_name']
+    record= json.loads(request.data)
+    record = record['voice']
     with open('audio.wav','wb') as file:
         binary = base64.b64decode(record)
         file.write(binary)
-    test = transcribe(file.name)
+    test = transcribe('audio.wav')
     test = str(test)
     converted_text = transliterate(test, sanscript.DEVANAGARI,sanscript.ITRANS)
-    converted_text = converted_text.lower()
-    return jsonify({'hindi':test,
-                    'hinglish':converted_text})
+    english = translator.translate(test).text
+    return jsonify({"Hindi": test,"Hinglish": converted_text,"English":english})
 
-if __name__ == '__main__':
-    app.run()
+if _name_ == '_main_':
+    app.run(host = '0.0.0.0',port = 8000)
